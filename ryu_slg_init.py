8fcd6c2685cd7295e835adff7ae37b2cb222c4fe
Rintaro Sekino <rintaro.sekino@ntti3.io>
2020-06-19 13:58:09 +0900

    Support arp

--- ryu_slg_init.py
+++ ryu_slg_init.py
@@ -24,6 +24,17 @@ from ryu.ofproto import ofproto_v1_3
 from ryu.ofproto.ether import ETH_TYPE_IP
 from ryu.lib.ofctl_v1_3 import mod_flow_entry
 
+# Added for SimpleArp
+from operator import attrgetter
+from ryu.controller.handler import MAIN_DISPATCHER
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import arp
+from ryu.lib.packet.packet import Packet
+from ryu.lib.packet.ethernet import ethernet
+from ryu.lib.packet.arp import arp
+from ryu.ofproto import ether
+from ryu.ofproto import inet
 
 
 #####################################################################################################################
@@ -95,7 +106,7 @@ class SliceGateway(dict):
 
     def __repl__(self):
         return self.interface
-    
+
     def add_interface(self, index, ip_address, mac_address):
         self.interface[index] = interface(index, ip_address, mac_address)
         return
@@ -125,8 +136,8 @@ def make_vxlan_decap_action():
         {"type": "DECAP", "cur_pkt_type": PACKET_TYPE_UDP, "new_pkt_type": PACKET_TYPE_VxLAN},
         {"type": "DECAP", "cur_pkt_type": PACKET_TYPE_VxLAN, "new_pkt_type": PACKET_TYPE_NEXT},
     ]
-        
-    
+
+
 slg = [
     SliceGateway(),
     SliceGateway(),
@@ -154,6 +165,7 @@ PACKET_TYPE_UDP=131089
 PACKET_TYPE_VxLAN=201397
 PACKET_TYPE_NEXT=65534
 
+
 class RyuSlgInit(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
@@ -162,45 +174,179 @@ class RyuSlgInit(app_manager.RyuApp):
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
+        msg = ev.msg
         datapath = ev.msg.datapath
         ofproto = datapath.ofproto
+        datapath.id = msg.datapath_id
+        ofproto_parser = datapath.ofproto_parser
         self.logger.info('switch joind: datapath: %061x' % datapath.id)
 
         slg_id = datapath.id - 1
-        
-        # base flow
-        flow = {'priority': 1, 'table_id': 0}
-        flow['match'] = {'in_port': 1}
-        flow['actions'] = [{'type':'OUTPUT','port': 2}]
-        print(flow)
-        mod_flow_entry(datapath, flow, ofproto.OFPFC_ADD)
-        flow['match'] = {'in_port': 2}
-        flow['actions'] = [{'type':'OUTPUT','port': 1}]
-        print(flow)
-        mod_flow_entry(datapath, flow, ofproto.OFPFC_ADD)
-        
-        if slg_id != 1 :
+        self.logger.info('slg_id: {}'.format(slg_id))
+
+        set_config = ofproto_parser.OFPSetConfig(
+            datapath,
+            datapath.ofproto.OFPC_FRAG_NORMAL,
+            datapath.ofproto.OFPCML_MAX
+        )
+        datapath.send_msg(set_config)
+        self.install_table_miss(datapath, datapath.id)
+
+        if slg_id != 1:
             # in_port: 1 ENCAP
-            flow = {'priority': 10, 'table_id':0}
-            flow['match'] = {'in_port': 1, "dl_type":2048}
+            flow = {'priority': 10, 'table_id': 0}
+            flow['match'] = {'in_port': 1, "dl_type": 2048}
             flow['actions'] = make_vxlan_encap_action(
-                eth_src = slg[slg_id].interface[2].mac_address,
-                eth_dst = slg[slg_id].interface[2].gateway.mac_address,
-                ipv4_src = slg[slg_id].interface[2].ip_address,
-                ipv4_dst = slg[2 if slg_id != 2 else 0].interface[2].ip_address,
-                udp_src = 12345,
-                vni = 5000 + 4 * 10
+                eth_src=slg[slg_id].interface[2].mac_address,
+                eth_dst=slg[slg_id].interface[2].gateway.mac_address,
+                ipv4_src=slg[slg_id].interface[2].ip_address,
+                ipv4_dst=slg[2 if slg_id != 2 else 0].interface[2].ip_address,
+                udp_src=12345,
+                vni=5000 + 4 * 10
             )
-            flow['actions'].append({"type":"OUTPUT", "port": 2})
+            flow['actions'].append({"type": "OUTPUT", "port": 2})
             print(flow)
-            #mod_flow_entry(datapath, flow, ofproto.OFPFC_ADD)
-        
+            mod_flow_entry(datapath, flow, ofproto.OFPFC_ADD)
+
         # in_port: 2 DECAP
-        flow = {'priority': 10, 'table_id':0}
-        flow['match'] = {'in_port':2, 'dl_type': 2048, "nw_proto":17, "tp_dst": 4789}
+        flow = {'priority': 10, 'table_id': 0}
+        flow['match'] = {
+                            'in_port': 2,
+                            'dl_type': 2048,
+                            "nw_proto": 17,
+                            "tp_dst": 4789
+                        }
         flow['actions'] = make_vxlan_decap_action()
-        flow['actions'].append({"type": "SET_FIELD", "field": "eth_dst", "value": slg[slg_id].interface[1].gateway.mac_address})
-        flow['actions'].append({"type":"OUTPUT", "port": 1})
+        flow['actions'].append({
+                        "type": "SET_FIELD",
+                        "field": "eth_dst",
+                        "value": slg[slg_id].interface[1].gateway.mac_address
+                    })
+        flow['actions'].append({"type": "OUTPUT", "port": 1})
         print(flow)
-        #mod_flow_entry(datapath, flow, ofproto.OFPFC_ADD)
-        
+        mod_flow_entry(datapath, flow, ofproto.OFPFC_ADD)
+
+        # Packet-in for ARP
+        flow = {'priority': 100, 'table_id': 0}
+        flow['match'] = {
+                'in_port': 1,
+                'dl_type': 2054,
+                'eth_dst': "ff:ff:ff:ff:ff:ff",
+                'ipv4_dst': slg[slg_id].interface[1].ip_address
+            }
+        flow['actions'] = [{"type": "OUTPUT", "port": 4294967293}]
+        print(flow)
+        mod_flow_entry(datapath, flow, ofproto.OFPFC_ADD)
+        flow['match'] = {
+                'in_port': 2,
+                'dl_type': 2054,
+                'eth_dst': "ff:ff:ff:ff:ff:ff",
+                'ipv4_dst': slg[slg_id].interface[2].ip_address
+            }
+        flow['actions'] = [{"type": "OUTPUT", "port": 4294967293}]
+        print(flow)
+        mod_flow_entry(datapath, flow, ofproto.OFPFC_ADD)
+
+    def install_table_miss(self, datapath, dpid):
+        datapath.id = dpid
+
+        match = datapath.ofproto_parser.OFPMatch()
+
+        actions = [datapath.ofproto_parser.OFPActionOutput(
+                datapath.ofproto.OFPP_CONTROLLER,
+                datapath.ofproto.OFPCML_NO_BUFFER)]
+        inst = [datapath.ofproto_parser.OFPInstructionActions(
+                datapath.ofproto.OFPIT_APPLY_ACTIONS, actions)]
+        mod = datapath.ofproto_parser.OFPFlowMod(
+                datapath=datapath,
+                priority=0,
+                buffer_id=0xffffffff,
+                match=match,
+                instructions=inst)
+        datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        inPort = msg.match['in_port']
+
+        packet = Packet(msg.data)
+        etherFrame = packet.get_protocol(ethernet)
+        if etherFrame.ethertype == ether.ETH_TYPE_ARP:
+            self.receive_arp(datapath, packet, etherFrame, inPort)
+            return 0
+        else:
+            self.logger.debug("Drop packet")
+            return 1
+
+    def receive_arp(self, datapath, packet, etherFrame, inPort):
+        arpPacket = packet.get_protocol(arp)
+
+        if arpPacket.opcode == 1:
+            arp_dstIp = arpPacket.dst_ip
+            self.logger.debug("receive ARP request %s => %s (port%d)"
+                       %(etherFrame.src, etherFrame.dst, inPort))
+            self.reply_arp(datapath, etherFrame, arpPacket, arp_dstIp, inPort)
+        elif arpPacket.opcode == 2:
+            pass
+
+    def reply_arp(self, datapath, etherFrame, arpPacket, arp_dstIp, inPort):
+        dstIp = arpPacket.src_ip
+        srcIp = arpPacket.dst_ip
+        dstMac = etherFrame.src
+        if datapath.id - 1 == 0:
+            if arp_dstIp == "192.168.42.254":
+                srcMac = "52:54:00:01:01:01"
+                outPort = 1
+            elif arp_dstIp == "172.16.1.1":
+                srcMac = "52:54:00:00:00:01"
+                outPort = 2
+            else:
+                self.logger.debug("unknown arp request received !")
+        if datapath.id - 1 == 1:
+            if arp_dstIp == "192.168.1.254":
+                srcMac = "52:54:00:02:01:01"
+                outPort = 1
+            elif arp_dstIp == "172.16.2.1":
+                srcMac = "52:54:00:00:00:02"
+                outPort = 2
+            else:
+                self.logger.debug("unknown arp request received !")
+        if datapath.id - 1 == 2:
+            if arp_dstIp == "192.168.3.254":
+                srcMac = "52:54:00:03:01:01"
+                outPort = 1
+            elif arp_dstIp == "172.16.3.1":
+                srcMac = "52:54:00:00:00:03"
+                outPort = 2
+            else:
+                self.logger.debug("unknown arp request received !")
+
+        self.send_arp(datapath, 2, srcMac, srcIp, dstMac, dstIp, outPort)
+        self.logger.debug("send ARP reply %s => %s (port%d)" %(srcMac, dstMac, outPort))
+
+    def send_arp(self, datapath, opcode, srcMac, srcIp, dstMac, dstIp, outPort):
+        if opcode == 1:
+            targetMac = "00:00:00:00:00:00"
+            targetIp = dstIp
+        elif opcode == 2:
+            targetMac = dstMac
+            targetIp = dstIp
+
+        e = ethernet(dstMac, srcMac, ether.ETH_TYPE_ARP)
+        a = arp(1, 0x0800, 6, 4, opcode, srcMac, srcIp, targetMac, targetIp)
+        p = Packet()
+        p.add_protocol(e)
+        p.add_protocol(a)
+        p.serialize()
+
+        actions = [datapath.ofproto_parser.OFPActionOutput(outPort, 0)]
+        out = datapath.ofproto_parser.OFPPacketOut(
+            datapath=datapath,
+            buffer_id=0xffffffff,
+            in_port=datapath.ofproto.OFPP_CONTROLLER,
+            actions=actions,
+            data=p.data)
+        datapath.send_msg(out)
